ce9625ae0f94ee9e6f3fa4bee67506f886a83cfd
mnltake <minolu2003@ybb.ne.jp>
2020-01-21 23:04:42 +0900

    inside for loop

--- neopixel.ino
+++ neopixel.ino
@@ -1,12 +1,14 @@
 void lightbar(float distanceFromLine ){
   int level = constrain (distanceFromLine * 0.1 / cmPerLightbarPixel , -centerpixcel ,centerpixcel);
   byte n = level + centerpixcel;
-  for (int i = 0 ;i < NUMPIXELS; i++){ //Clear All pixel
-    pixels.setPixelColor(i,0,0,0);
-  }
-  if (distanceFromLine != 32020){ //autosteer ON
-    pixels.setPixelColor(n, pixels.Color(levelcolor[n][0],levelcolor[n][1],levelcolor[n][2])); //Light Bar
-    pixels.setPixelColor(centerpixcel,10,10,0);//Yellow center
+  for (int i = 0 ;i < NUMPIXELS; i++){
+    if (i==centerpixcel && distanceFromLine != 32020){
+      pixels.setPixelColor(centerpixcel,10,10,0);//Yellow center
+    }else if (i == n && distanceFromLine != 32020){
+      pixels.setPixelColor(n, pixels.Color(levelcolor[n][0],levelcolor[n][1],levelcolor[n][2])); //Light Bar
+    }else{
+      pixels.setPixelColor(i,0,0,0);//Clear
+    }
   }
   pixels.show();
 }
