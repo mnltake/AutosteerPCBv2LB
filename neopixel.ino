8ea32521cd2b786c6ccfffb638491b5abe8d1c65
mnltake <minolu2003@ybb.ne.jp>
2020-01-24 09:47:50 +0900

    Light Bar renewal

--- neopixel.ino
+++ neopixel.ino
@@ -17,11 +17,10 @@ void lightbar(float distanceFromLine ){
   int level = constrain (distanceFromLine * 0.1 / cmPerLightbarPixel , -centerpixcel ,centerpixcel);
   byte n = level + centerpixcel;
     for (int i = 0 ;i < NUMPIXELS; i++){
-      if (i == centerpixcel && i == n){
-        pixels.setPixelColor(i,levelcolor[i][0],levelcolor[i][1],levelcolor[i][2]);
-      }else if (level < 0 && i >= n && i < centerpixcel && distanceFromLine != 32020){
-        pixels.setPixelColor(i,levelcolor[i][0],levelcolor[i][1],levelcolor[i][2]);
-      }else if (level > 0 && i <= n && i > centerpixcel && distanceFromLine != 32020){
+      if ( (i == centerpixcel && i == n)|| //Center
+           (level < 0 && i >= n && i < centerpixcel && distanceFromLine != 32020)|| //Right Bar
+           (level > 0 && i <= n && i > centerpixcel && distanceFromLine != 32020) ) //Left Bar
+      {
         pixels.setPixelColor(i,levelcolor[i][0],levelcolor[i][1],levelcolor[i][2]);
       }else{
         pixels.setPixelColor(i,0,0,0);//Clear
